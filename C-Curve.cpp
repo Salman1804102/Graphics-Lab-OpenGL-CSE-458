cf3b1d8f3ee03aeda6179be5b4adc2e647668150
Salman Farsi <62229105+Salman1804102@users.noreply.github.com>
2023-08-13 00:38:03 +0600

    Update C-Curve.cpp

--- C-Curve.cpp
+++ C-Curve.cpp
@@ -1,28 +1,19 @@
-#include <GL/glut.h>
 #include<bits/stdc++.h>
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-void DrawKochCurve(float startX, float startY, float len, float angle, int n)
+#include <GL/glut.h>
+#define PI 3.14159265358979323846
+void DrawCCurve(float startX, float startY, float len, float angle, int n)
 {
     if (n > 0)
     {
-        len = len / 3.0;
-        DrawKochCurve(startX, startY, len, angle, n - 1);
-        startX += len * cos(angle);
-        startY += len * sin(angle);
-        DrawKochCurve(startX, startY, len, angle - M_PI / 3.0, n - 1);
-        startX += len * cos(angle - M_PI / 3.0);
-        startY += len * sin(angle - M_PI / 3.0);
-        DrawKochCurve(startX, startY, len, angle + M_PI / 3.0, n - 1);
-        startX += len * cos(angle + M_PI / 3.0);
-        startY += len * sin(angle + M_PI / 3.0);
-        DrawKochCurve(startX, startY, len, angle, n - 1);
+        len = len / sqrt(2);
+        DrawCCurve(startX, startY, len, angle + PI / 4, n - 1);
+        startX = startX + len * cos(angle + PI / 4);
+        startY = startY + len * sin(angle + PI / 4);
+        DrawCCurve(startX, startY, len, angle - PI / 4, n - 1);
     }
     else
     {
         glBegin(GL_LINES);
-        glColor3f(1.0, 0.0, 0.0);
         glVertex2f(startX, startY);
         float endX = startX + len * cos(angle);
         float endY = startY + len * sin(angle);
@@ -34,29 +25,29 @@ void DrawKochCurve(float startX, float startY, float len, float angle, int n)
 void Display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 1.0, 0.0);
+    glColor3f(1.0, 0.0, 0.0);
 
-    float startX = -0.5;
-    float startY = -0.3;
-    float len = 1.0;
+    float startX = -0.4;
+    float startY = 0.0;
+    float len = 0.8;
     float angle = 0.0;
 
     int n = 3;
 
-    DrawKochCurve(startX, startY, len, angle, n);
+    DrawCCurve(startX, startY, len, angle, n);
 
     glFlush();
 }
-
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(800, 600);
-    glutCreateWindow("Koch Curve");
+    glutCreateWindow("C Curve");
     glClearColor(1.0, 1.0, 1.0, 1.0);
     gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
     glutDisplayFunc(Display);
     glutMainLoop();
     return 0;
 }
+
